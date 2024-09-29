0eab95f3d9a212a9495476949f8505b876337266
Gza Estrada <gerrylizarestrada@gmail.com>
2024-09-29 21:46:15 +0800

    contact_us

--- resources/js/components/ContactUs.js
+++ resources/js/components/ContactUs.js
@@ -3,8 +3,57 @@ import NavList from "./Navlist";
 
 export default function ContactUs() {
     return (
-        <div className="Aboutus">
-            <NavList />
+        <div className="ContactUs">
+            {/* Navigation Bar Component */}
+            <div className="navbar">
+                <NavList />
+            </div>
+
+            {/* Contact Information Section */}
+            <section className="contact-info">
+                <h1>Contact Us</h1>
+                <h2>Get in Touch!</h2>
+                <p>
+                    We are eager to hear from you! Please don’t hesitate to
+                    reach out with any questions, feedback, or concerns you may
+                    have. Your thoughts are important to us, and we look forward
+                    to assisting you.
+                </p>
+
+                {/* Stats Section */}
+                <div className="stats-section">
+                    <div className="stat-item">
+                        <h3>Email:</h3>
+                        <p>
+                            <a href="mailto:info@ourschool.edu">
+                                xxx@school.edu
+                            </a>
+                        </p>
+                    </div>
+                    <div className="stat-item">
+                        <h3>Phone:</h3>
+                        <p>
+                            <a href="tel:+631234567890">+63 123 456 7890</a>
+                        </p>
+                    </div>
+                    <div className="stat-item">
+                        <h3>Address:</h3>
+                        <p>
+                            123 ABC, Brgy XYZ
+                            <br />
+                            Butuan City, Philippines, 8600
+                        </p>
+                    </div>
+                    <div className="stat-item">
+                        <h3>Office Hours:</h3>
+                        <p>
+                            Monday to Friday:
+                            <br />
+                            8:00 AM - 5:00 PM
+                        </p>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
--- resources/js/components/Navlist.js
+++ resources/js/components/Navlist.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavList() {
+    const location = useLocation(); // Get the current location
+
     return (
         <div className="nav-list">
-            <Link to="/">Home</Link> <br />
-            <Link to="/about-us">About Us</Link> <br />
-            <Link to="/contact-us">Contact Us</Link> <br />
-            <Link to="/login">Login</Link> {/* New link to Login page */}
+            {location.pathname !== "/" && (
+                <>
+                    <Link to="/">Home</Link> <br />
+                </>
+            )}
+            {location.pathname !== "/about-us" && (
+                <>
+                    <Link to="/about-us">About Us</Link> <br />
+                </>
+            )}
+            {location.pathname !== "/contact-us" && (
+                <>
+                    <Link to="/contact-us">Contact Us</Link> <br />
+                </>
+            )}
+            {location.pathname !== "/login" && (
+                <>
+                    <Link to="/login">Login</Link> <br />
+                </>
+            )}
         </div>
     );
 }
